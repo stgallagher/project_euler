e9f5f5d7c917ff8a3ed70966c393396fe0c8088c
Steven Gallagher <s.thomas.gallagher@gmail.com>
2012-07-02 17:50:08 -0500

    problem 11: square parceling

--- lib/project_euler/problem_11.rb
+++ lib/project_euler/problem_11.rb
@@ -25,5 +25,48 @@ class ProblemEleven
     integers
   end
 
+  def square_parceler(square_length, grid)
+    square_sample = []
+    grid_length = grid_length_finder(square_length, grid)
+    upper_right_corner = 0
+    while upper_right_corner < (grid.size - (((square_length - 1) * grid_length) + square_length))
+      while upper_right_corner % grid_length <= (grid_length - square_length)
+        fill_square(square_sample, grid, upper_right_corner, square_length, grid_length)
+      end
+    end
+    square_sample
+  end
+
+  def grid_length_finder(length, grid)
+    grid_length = Math.sqrt(grid.size/length**2) * length
+  end
+
+  def fill_square(square, grid, right_corner, square_length, grid_length)
+    start = right_corner
+    (square_length).times do
+      (0..square_length - 1).each do |offset|
+        fill_row(square, start, offset, grid)
+      end
+      start += grid_length
+    end
+    square
+  end
+
+  def fill_row(square, start, offset, grid)
+    square << grid[start + offset]
+  end
+
+  def row_multiplier(square)
+  end
+
+  def col_multiplier(square)
+  end
+
+  def upward_diagonal_multiplier(square)
+  end
+
+  def downward_diagonal_multiplier(square)
+  end
+
 end
 
--- spec/project_euler/problem_11_spec.rb
+++ spec/project_euler/problem_11_spec.rb
@@ -4,6 +4,7 @@ describe ProblemEleven do
 
   before(:each) do
     @p11 = ProblemEleven.new
+    @grid = @p11.format_contents(@p11.build_array_from_file("grid_numbers.txt"))
   end
 
   it "builds an array of values from a file" do
@@ -14,5 +15,20 @@ describe ProblemEleven do
     preformatted_array = ["08 02 20 48", "27 05 82 32", "12 43 61 04"]
     @p11.format_contents(preformatted_array).should == [8, 2, 20, 48, 27, 5, 82, 32, 12, 43, 61, 4]
   end
+
+  it "parcels a square set out of a larger square set" do
+    pending
+    @p11.square_parceler(4, @grid).size.should == 16
+  end
+
+  it "determines total side length of a grid based on the size of the side of its component parcels" do
+    @p11.grid_length_finder(4, @grid).should == 20
+  end
+
+  it "fills a square with the appropriate numbers" do
+    square = []
+    @p11.fill_square(square, @grid, 0, 4, 20).should == [8, 2, 22, 97, 49, 49, 99, 40, 81, 49, 31, 73, 52, 70, 95, 23]
+  end
+
 
 end
