e8518dff1e092d214c84d72fceb05a32d6718d38
Steven Gallagher <s.thomas.gallagher@gmail.com>
2012-07-05 07:41:52 -0500

    solved problem 11

--- lib/project_euler/problem_11.rb
+++ lib/project_euler/problem_11.rb
@@ -26,15 +26,32 @@ class ProblemEleven
   end
 
   def square_parceler(square_length, grid)
+    max = 0
+    sample_max = 0
+    col_level = 0
+    first_row = true
     square_sample = []
     grid_length = grid_length_finder(square_length, grid)
-    upper_right_corner = 0
-    while upper_right_corner < (grid.size - (((square_length - 1) * grid_length) + square_length))
-      while upper_right_corner % grid_length <= (grid_length - square_length)
-        fill_square(square_sample, grid, upper_right_corner, square_length, grid_length)
+    grid.each_index do |idx|
+      if index_determiner(idx, square_length, grid_length, grid)
+        square_sample = fill_square(square_sample, grid, idx, square_length, grid_length)
+        sample_max = parcel_max_product(square_sample, square_length)
+        max = sample_max if sample_max > max
+        square_sample.clear
       end
     end
-    square_sample
+    max
+  end
+
+  def index_determiner(idx, square_length, grid_length, grid)
+    relative_idx = idx % grid_length
+    row_limit = grid_length - square_length
+    column_limit = grid.size - (((square_length - 1) * grid_length) + square_length)
+    if(relative_idx <= row_limit && idx < column_limit)
+      return idx
+    else
+      return nil
+    end
   end
 
   def grid_length_finder(length, grid)
@@ -56,16 +73,63 @@ class ProblemEleven
     square << grid[start + offset]
   end
 
-  def row_multiplier(square)
+  def row_max_multiplier(square, square_length)
+    max = 0
+    square.each_slice(square_length) do |row|
+      row_max = row.inject { |product, factor| product *= factor }
+      max = row_max if row_max > max
+    end
+    max
   end
 
-  def col_multiplier(square)
+  def col_max_multiplier(square, square_length)
+    max = 0
+    col = []
+    (square_length).times do
+      (0..square_length - 1).each do |offset|
+        (0..square_length - 1).each do |col_level|
+          col << square[offset + (col_level * square_length)]
+          col_max = col.inject { |product, factor| product *= factor }
+          max = col_max if col_max > max
+        end
+        col.clear
+      end
+    end
+    max
   end
 
-  def upward_diagonal_multiplier(square)
+  def upward_diagonal_max_multiplier(square, square_length)
+    diag_max = 0
+    diag = []
+    (0..square_length - 1).each do |offset|
+      diag << square[((square_length - 1) - offset) + (offset * square_length)]
+      diag_max = diag.inject { |product, factor| product *= factor }
+    end
+    diag_max
   end
 
-  def downward_diagonal_multiplier(square)
+  def downward_diagonal_max_multiplier(square, square_length)
+    diag_max = 0
+    diag = []
+    (0..square_length - 1).each do |offset|
+      diag << square[ offset + (offset * square_length)]
+      diag_max = diag.inject { |product, factor| product *= factor }
+    end
+    diag_max
+  end
+
+  def parcel_max_product(square,square_length)
+    max = 0
+    max = row_max_multiplier(square,square_length) if row_max_multiplier(square,square_length) > max
+    max = col_max_multiplier(square,square_length) if col_max_multiplier(square,square_length) > max
+    max = upward_diagonal_max_multiplier(square,square_length) if upward_diagonal_max_multiplier(square,square_length) > max
+    max = downward_diagonal_max_multiplier(square,square_length) if downward_diagonal_max_multiplier(square,square_length) > max
+    max
+  end
+
+  def problem_11_solver(filename, square_length)
+    grid = format_contents(build_array_from_file(filename))
+    square_parceler(square_length, grid)
   end
 
 end
--- spec/project_euler/problem_11_spec.rb
+++ spec/project_euler/problem_11_spec.rb
@@ -5,6 +5,8 @@ describe ProblemEleven do
   before(:each) do
     @p11 = ProblemEleven.new
     @grid = @p11.format_contents(@p11.build_array_from_file("grid_numbers.txt"))
+    square = []
+    @square = @p11.fill_square(square, @grid, 0, 4, 20)
   end
 
   it "builds an array of values from a file" do
@@ -17,8 +19,7 @@ describe ProblemEleven do
   end
 
   it "parcels a square set out of a larger square set" do
-    pending
-    @p11.square_parceler(4, @grid).size.should == 16
+    @p11.square_parceler(4, @grid).should == 70600674
   end
 
   it "determines total side length of a grid based on the size of the side of its component parcels" do
@@ -30,5 +31,28 @@ describe ProblemEleven do
     @p11.fill_square(square, @grid, 0, 4, 20).should == [8, 2, 22, 97, 49, 49, 99, 40, 81, 49, 31, 73, 52, 70, 95, 23]
   end
 
+  it "finds the max of the rows multiplied" do
+    @p11.row_max_multiplier(@square, 4).should == 9507960
+  end
+
+  it "finds the max of the cols multiplied" do
+    @p11.col_max_multiplier(@square, 4).should == 6514520
+  end
+
+  it "finds the max of the upward diagonal multiplied" do
+    @p11.upward_diagonal_max_multiplier(@square, 4).should == 24468444
+  end
+
+  it "finds the max of the downward diagonal multiplied" do
+    @p11.downward_diagonal_max_multiplier(@square, 4).should == 279496
+  end
+
+  it "finds the max product of a single line of a parcel, attempting products of all rows, cols, and diagonals" do
+    @p11.parcel_max_product(@square, 4).should == 24468444
+  end
+
+  it "solves problem 11 of Project Euler" do
+    @p11.problem_11_solver("grid_numbers.txt", 4).should == 70600674
+  end
 
 end
